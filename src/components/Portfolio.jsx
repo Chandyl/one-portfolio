b6de173cc94ce2c909e537362a06cc2161759ad3
Stacee <swilliams134@collin.edu>
2022-06-17 18:56:37 -0500

    refactored

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,15 +25,15 @@ const Portfolio = () => {
 						{/* Hover Effects */}
 						<div className="opacity-0 group-hover:opacity-100">
 							<span className=" text-center text-md font-bold text-gray-300 tracking-wider pl-6 pt-2 ml-6">
-								Coming Soon
+								AirTravel HTML/CSS only
 							</span>
 							<div className="pt-8 text-center">
-								<a href="/">
+								<a href="https://chandyl.github.io/travels/">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-gray-400 text-[#f1faee] font-bold text-lg">
 										Demo
 									</button>
 								</a>
-								<a href="/">
+								<a href="https://github.com/Chandyl/travels">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-gray-400 text-[#f1faee] font-bold text-lg">
 										Code
 									</button>
